6801b315e967a4b2ede87b50f484a8c27d52a14e
lofayo <870876711@qq.com>
2018-07-13 11:26:22 +0800

    finally

--- pages/myMovie/myMovieData.js
+++ pages/myMovie/myMovieData.js
@@ -1,41 +1,35 @@
 const myMovieData = [{
-  id: 0,
-  name: '神秘巨星',
-  poster: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2508925590.webp',
-  video_url: 'http://140.143.16.172/SecretSuperstar.mkv',
-  description: '少女伊西亚（塞伊拉·沃西 Zaira Wasim 饰）拥有着一副天生的好嗓子，对唱歌充满了热爱的她做梦都想成为一名歌星。然而，伊西亚生活在一个不自由的家庭之中，母亲娜吉玛（梅·维贾 Meher Vij 饰）常常遭到性格爆裂独断专横的父亲法鲁克（拉杰·阿晶 Raj Arjun 饰）的拳脚相向，伊西亚知道，想让父亲支持自己的音乐梦想是完全不可能的事情。 　　某日，母亲卖掉了金项链给伊西亚买了一台电脑，很快，伊西亚便发现，虽然无法再现实里实现梦想，但是网络中存在着更广阔的舞台。伊西亚录制了一段蒙着脸自弹自唱的视屏上传到了优兔网上，没想到收获了异常热烈的反响，著名音乐人夏克提（阿米尔·汗 Aamir Khan 饰）亦向她抛出了橄榄枝。'
-},  {
+    id: 0,
+    name: '神秘巨星',
+    poster: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2508925590.webp',
+    video_url: 'http://140.143.16.172/video/SecretSuperstar.mkv',
+    description: '少女伊西亚（塞伊拉·沃西 Zaira Wasim 饰）拥有着一副天生的好嗓子，对唱歌充满了热爱的她做梦都想成为一名歌星。然而，伊西亚生活在一个不自由的家庭之中，母亲娜吉玛（梅·维贾 Meher Vij 饰）常常遭到性格爆裂独断专横的父亲法鲁克（拉杰·阿晶 Raj Arjun 饰）的拳脚相向，伊西亚知道，想让父亲支持自己的音乐梦想是完全不可能的事情。 　　某日，母亲卖掉了金项链给伊西亚买了一台电脑，很快，伊西亚便发现，虽然无法再现实里实现梦想，但是网络中存在着更广阔的舞台。伊西亚录制了一段蒙着脸自弹自唱的视屏上传到了优兔网上，没想到收获了异常热烈的反响，著名音乐人夏克提（阿米尔·汗 Aamir Khan 饰）亦向她抛出了橄榄枝。'
+}, {
     id: 1,
-    name: '小萝莉的猴神大叔',
-    poster: 'https://img3.doubanio.com/view/photo/l/public/p2510956726.webp',
-    video_url: 'http://pcvideows.titan.mgtv.com/c1/2018/04/11_0/0FA95E5EEC3F1F034CDC5E9154CBA4CB_20180411_1_1_722.mp4?arange=0&pm=cQwP__IBjPIZki4~ateTEbxbKecmyAMS8NFgGbmGBdco1L2FLssombf4rLbMo3AW_vkM7MVBEum0RK905c0I9X5HZoYdNGNkQdXVijW~zAaOSbV6TERozWQ6mSiyXRJtYCO9GSOgDLRpnoBeJ61f6d7m3TB107BMq2MRc04hzAZO9aNZAQXvJKZdHRwX4~C_yNiJc9ISilgbYzS41HagSRvYXDHeFIbqHFg~5~J8oVxoJbXDd5NH0rSCmiskFYFYB5G_KwAxvkSMZXbYA2aVDEhYHlxfG7Gk_j3O1sa4D9HcYt4IYOGZA~ALfuWHn5~qW8ahxuLuMCDpJP1Qqo3~ri2DSNDhyXfMKCAiLYGd4alrvdXaAE~5ovmYYn9Ni49ZEK0uPM3Kx7DyXR~HHDHgkg--',
-    description: '一对印度的中产阶级夫妇:服装店老板拉吉（伊尔凡·可汗 Irrfan Khan 饰）与太太米塔（萨巴·卡玛尔 Saba Qamar 饰）为了让女儿皮娅（蒂希塔·塞加尔 Dishita Sehgal 饰）接受更好的教育想尽了各种办法。而当他们费劲心思终于要将女儿送进名校时,事情却又发生了意想不到的变化……'
+    name: '三傻大闹宝莱坞 3 Idiots',
+    poster: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p579729551.webp',
+    video_url: 'http://140.143.16.172/video/threeIdiots.rmvb',
+    description: '本片根据印度畅销书作家奇坦·巴哈特（Chetan Bhagat）的处女作小说《五点人》（Five Point Someone）改编而成。法兰（马德哈万 R Madhavan 饰）、拉杜（沙曼·乔希 Sharman Joshi 饰）与兰乔（阿米尔·汗 Aamir Khan 饰）是皇家工程学院的学生，三人共居一室，结为好友。在以严格著称的学院里，兰乔是个非常与众不同的学生，他不死记硬背，甚至还公然顶撞校长“病毒”（波曼·伊拉尼 Boman Irani 饰），质疑他的教学方法。他不仅鼓动法兰与拉杜去勇敢追寻理想，还劝说校长的二女儿碧雅（卡琳娜·卡普 Kareena Kapoor 饰）离开满眼铜臭的未婚夫。兰乔的特立独行引起了模范学生“消音器”（奥米·维嘉 Omi Vaidya 饰）的不满，他约定十年后再与兰乔一决高下，看哪种生活方式更能取得成功。 '
 }, {
-  id: 3,
-  name: '摔跤吧！爸爸',
-  poster: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2457983084.webp',
-  video_url: 'http://pcvideogs.titan.mgtv.com/c1/2017/07/04_0/D37570B18EB50AF1EDC92A3A24A07248_20170704_1_1_776.mp4?arange=0&pm=1a5AAKKof~2e2Dfg0ZsRg6zVCKrAqE4aRT~uaigHJVmMl5vAPwQ0XWcHO24TYFluoSFsZh_o0f4rgwrBCwEGFN6PZumyJaWQ_WV0UauwKF~c~n9pW_6Dj7JWrClxH3FaUitLkYgyeX4UKvkkT3FTaKIGa~eBPDpoft2hnNW7W3LseMMVMQwAYt_p26HC~Tge4wsuxfplz~wKsadyIN082RkbMnq7L3LiUUf8N3XY5DK~H19acHYHy2GRLGhZC9cM_R6DKMii1IHcLQgiKeZkLKnSCwnT~Q2swP5Ljjmj82ZWCXlWJY8rW7mNvPSaMmlNvQgDUPRolwqv2crdIVbsXG0VnTOKTZHPePQ5Lfjt7wShctaDlhW~nGiPtCdlKKWDKW6j7pkXD9eIY5Qw_bx89w--',
-  description: '马哈维亚（阿米尔·汗 Aamir Khan 饰）曾经是一名前途无量的摔跤运动员，在放弃了职业生涯后，他最大的遗憾就是没有能够替国家赢得金牌。马哈维亚将这份希望寄托在了尚未出生的儿子身上，哪知道妻子接连给他生了两个女儿，取名吉塔（法缇玛·萨那·纱卡 Fatima Sana Shaikh 饰）和巴比塔（桑亚·玛荷塔 Sanya Malhotra 饰）。让马哈维亚没有想到的是，两个姑娘展现出了杰出的摔跤天赋，让他幡然醒悟，就算是女孩，也能够昂首挺胸的站在比赛场上，为了国家和她们自己赢得荣誉。 　　就这样，在马哈维亚的指导下，吉塔和巴比塔开始了艰苦的训练，两人进步神速，很快就因为在比赛中连连获胜而成为了当地的名人。为了获得更多的机会，吉塔进入了国家体育学院学习，在那里，她将面对更大的诱惑和更多的选择。'
-  }, {
+    id: 2,
+    name: '摔跤吧！爸爸',
+    poster: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2457983084.webp',
+    video_url: 'http://140.143.16.172/video/WrestlingCompetition.mp4',
+    description: '马哈维亚（阿米尔·汗 Aamir Khan 饰）曾经是一名前途无量的摔跤运动员，在放弃了职业生涯后，他最大的遗憾就是没有能够替国家赢得金牌。马哈维亚将这份希望寄托在了尚未出生的儿子身上，哪知道妻子接连给他生了两个女儿，取名吉塔（法缇玛·萨那·纱卡 Fatima Sana Shaikh 饰）和巴比塔（桑亚·玛荷塔 Sanya Malhotra 饰）。让马哈维亚没有想到的是，两个姑娘展现出了杰出的摔跤天赋，让他幡然醒悟，就算是女孩，也能够昂首挺胸的站在比赛场上，为了国家和她们自己赢得荣誉。　就这样，在马哈维亚的指导下，吉塔和巴比塔开始了艰苦的训练，两人进步神速，很快就因为在比赛中连连获胜而成为了当地的名人。为了获得更多的机会，吉塔进入了国家体育学院学习，在那里，她将面对更大的诱惑和更多的选择。'
+}, {
+    id: 3,
+    name: '地球上的星星 Taare Zameen Par',
+    poster: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p1973489335.webp',
+    video_url: 'http://140.143.16.172/video/LikeStarsonEarth.mp4',
+    description: '对于8岁的男孩伊夏（达席尔·萨法瑞 Darsheel Safary 饰）来说，世界是充满了惊奇和快乐的万花筒，他正在用一切他能够想到的方式和这个陌生的世界进行着交流，同时也充分的享受着大地万物慷慨的赠与。可是，这样的伊夏却是成年人眼中的问题儿童，他的成绩不好，在班上的排名靠后， 脑子里还充满了各种匪夷所思的鬼点子，在又一次闯下大祸后，忍无可忍的父母将他送往了寄宿学校。 　　虽然伊夏的新生活并没有什么改变，但在内心里，和父母分离的生活让他感到闷闷不乐，这时，一位名叫尼克（阿米尔·汗 Aamir Khan 饰）的美术老师走进了他的生活。和以往所见到的固守成规的老师不同，尼克主张让学生们保留自己的个性和思想，自由的发展。在和尼克相处的日子里，伊夏和尼克都慢慢的成熟了起来。。'
+}, {
     id: 4,
-    name: '触不可及',
-    poster: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p1454261925.webp',
-    video_url: 'http://1253796429.vod2.myqcloud.com/3561640fvodgzp1253796429/c738bbba7447398156587779301/r5Mwjtt1ak0A.mp4?t=5b224d01&us=kdmaterial&sign=a8471144757961d31c7f3e00916bc885',
-    description: '因为一次跳伞事故，白人富翁菲利普Philippe（弗朗索瓦·克鲁塞 François Cluzet 饰）瘫痪在床，欲招聘一名全职陪护。由于薪酬高，应聘者云集，个个舌灿莲花，却无法打动他的心。直到黑人德希斯Driss（奥玛·赛 Omar Sy 饰）的出现才让他作出决定。德希斯刚从监狱出来，背负家庭重担，一心只想寻张辞退信以申领救济金，而且他明显对女助理的兴趣要远大于这份工作。但是菲利普还是一眼相中了这个小伙子。于是，德希斯开始了一个月的试用期。虽然舒适的豪宅环境让他倍感虚荣，但是他仍面临很多挑战：不仅要为菲利普作身体复健，还得给他洗浴、灌肠、拆信、穿丝袜等。起初，两人的思维方式与价值观大相径庭，但是，随着了解的不断深入，他们成为了朋友…… 　　本片取材于真实事件，获2011年东京电影节最佳影片金麒麟奖，弗朗索瓦·克鲁塞与奥玛·赛分享影帝桂冠。'
-}, {
-  id: 5,
-  name: '南极大冒险',
-  poster: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p687048224.webp',
-  video_url: 'http://1253796429.vod2.myqcloud.com/3561640fvodgzp1253796429/57c17eb37447398156589360054/Hba3eufZfa8A.mp4?t=5b225153&us=kdmaterial&sign=2f2f67d3b2a15e89ecf0ee6afab38c5e',
-  description: '动物与人之间互救互助，不舍不弃的感人故事，冰天雪地的南极迎来了三个科考队员，他们要前来搜寻一块陨石，随行的还有八条雪橇狗。南极风景优美，三人惊叹不已，然而危险却也随着美景到来。几天之后，一场暴风雪让他们尝到了苦头，其中一名队员掉进了薄冰下的冰水里面，危在旦夕，幸得雪橇狗奋身相救，队员才脱险——这似乎只是一个小小的序曲，更严峻的考验还在后头。 　　狂暴的风雪在肆虐着南极大地，科考队员进退两难，在恶劣的天气和环境下挣扎求生。而此时，他们只能决定放弃那八条忠实的雪橇狗。当三人安全返回的时候，他们怎样也放不下那些还陷于危难的狗。为了挽救它们，三人沿途折回，回到了那片南极大地。'
-  }, {
-    id: 6,
-    name: '弱点',
-    poster: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2181613001.webp',
-    video_url: 'http://58.220.50.132/videos/v0/20161208/0f/3a/4ab40ad91f67edc408c9288fe95623d4.mp4?key=02167b15716070198fb34e556bb67847b&dis_k=21b7a298c01cd12b34f0f90d72e764a6c&dis_t=1528957118&dis_dz=CT-ZheJiang_NingBo&dis_st=44&qd_k=5630126fdb043281577e4cc80df95979&qd_ip=73d560da&qd_src=02020031010000000000&qd_vip=1&qd_uid=1585254533&qd_tm=1528957116926',
-    description: '黑人男孩奥赫（昆东•亚伦 Quinton Aaron 饰）自幼父母离异，无家可归。不过，木讷的他却因为极强的身体条件和运动天赋，幸运地进入了一家孤儿院。虽然，他科科零分，但是一些细节却让他显得与众不同。一次排球比赛后，他主动收拾垃圾的行为，引起了陶西一家的注意。于是，陶西太 太（桑德拉•布洛克 Sandra Bullock 饰）决定收养奥赫，并把他培养成橄榄球选手。陶西太太让他感受到了家庭的温暖。同时，陶西的儿子还帮助奥赫训练，使他很快地融入了橄榄球队的生活，不断激发运动的潜能。终于，在一次比赛中，他因出色的表现崭露头角，开始获得了球探的关注，随之带来了一系列的甜蜜和麻烦…… 　　本片根据迈克尔•刘易斯（Michael Lewis）的原著《弱点：比赛进程》（The Blind Side: Evolution of the Game）改编，取材自2009年..'
-  }]
+    name: '肖申克的救赎 The Shawshank Redemption',
+    poster: 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p480747492.webp',
+    video_url: 'http://140.143.16.172/video/Shawshank.mkv',
+    description: '20世纪40年代末，小有成就的青年银行家安迪（蒂姆·罗宾斯 Tim Robbins 饰）因涉嫌杀害妻子及她的情人而锒铛入狱。在这座名为肖申克的监狱内，希望似乎虚无缥缈，终身监禁的惩罚无疑注定了安迪接下来灰暗绝望的人生。未过多久，安迪尝试接近囚犯中颇有声望的瑞德（摩根·弗里曼 Morgan Freeman 饰），请求对方帮自己搞来小锤子。以此为契机，二人逐渐熟稔，安迪也仿佛在鱼龙混杂、罪恶横生、黑白混淆的牢狱中找到属于自己的求生之道。他利用自身的专业知识，帮助监狱管理层逃税、洗黑钱，同时凭借与瑞德的交往在犯人中间也渐渐受到礼遇。表面看来，他已如瑞德那样对那堵高墙从憎恨转变为处之泰然，但是对自由的渴望仍促使他朝着心中的希望和目标前进。而关于其罪行的真相，似乎更使这一切朝前推进了一步…… '
+}]
 
 module.exports = {
-  myMovieData
+    myMovieData
 }
